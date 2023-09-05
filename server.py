2ec0a6dfe14585f3d8975c372f3e02e73b0baed9
Shuban <67974101+shuban-789@users.noreply.github.com>
2023-09-05 15:25:05 -0700

    Update server.py

--- server.py
+++ server.py
@@ -9,13 +9,22 @@ def hash_password(passw):
     return crypt.crypt(passw)
 
 def shell(client_socket):
+    client_socket.send(b"Shell access granted. Type 'exit' to exit the shell.\n")
+    shell_process = subprocess.Popen(['/bin/bash'], shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     while True:
         command = client_socket.recv(1024).decode("utf-8")
         if command.lower() == "exit":
             break
-        output = os.popen(command).read()
+        shell_process.stdin.write(command.encode("utf-8") + b'\n')
+        shell_process.stdin.flush()
+        output = shell_process.stdout.readline().decode("utf-8")
         client_socket.send(output.encode("utf-8"))
-
+    shell_process.stdin.close()
+    shell_process.stdout.close()
+    shell_process.stderr.close()
+    shell_process.kill()
+    client_socket.send(b"Shell session closed.\n")
+    
 def authenticate(client_socket, client_address):
     client_socket.send(b"Enter the username: ")
     username = client_socket.recv(1024).strip().decode("utf-8")
